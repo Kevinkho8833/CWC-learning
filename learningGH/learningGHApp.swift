22bdc467e6e60587e8a839e34ff85688c4b36f3b
Kevin Kho <kevinkho8833@gmail.com>
2021-07-07 05:28:11 +0800

    Initial Commit

---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by Kevin Kho Ka Yong on 07/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
