5e0533fb0b9e0597a955de1671556becfd222404
Kevin Kho <kevinkho8833@gmail.com>
2021-07-07 15:30:50 +0800

    add vstack

--- learningGH/ContentView.swift
+++ learningGH/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Kevin")
-            .padding()
+        
+        VStack {
+            Text("Hello, Kevin")
+                .padding()
+            Text("Hello, Eva")
+                .padding()
+        }
+        
     }
 }
 
